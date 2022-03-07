8c1dff3353e9429e4e90b6ee1141504c170486c9
DeadPumpkin <87383568+DeadPumpkin@users.noreply.github.com>
2022-03-07 00:25:09 +0000

    Add files via upload

---
+++ Slurß.py
@@ -0,0 +1,13 @@
+import keyboard, time, random, string
+a = ["Chilf", "felch", "Alabama hot pocket", "cunt", "skullfuck", "cum dumpster", "cock-juggling thunder cunt", "Cleveland Steamer", "blumpkin", "rusty trombone", "felcher", "soggy muffin", "glass bottom boat", "suck a fat baby's dick", "cunted", "fuck hole", "Monday morning quarterback", "hairy axe wound", "meat flap", "assmucus", "meat tulips", "Fuck girl", "cum chugger", "cunt-fuck", "beef curtain", "paedo" ,"cumdump", "fuckbitch", "cum guzzling cock sucker", "hang brains", "mother fucker", "fuckmeat", "bitchass mother fucker", "fuck me in the ass with no Vaseline", "fubugly", "snork", "fist fuck", "roast beef curtains", "motherfucker", "eat hair pie", "dick cheese", "motherfucking", "cumjunkie", "clitty litter", "50 yard cunt punt", "could suck a tennis ball through a garden hose", "labe", "schlong juie", "get some squish", "chota bags", "blue waffle", "ham flap", "meat drapes", "HPFM", "squeeze a steamer", "fucko", "dick hole", "GMILF", "ass fuck", "butt fuck", "gobble knob", "skronk", "cock snot", "cunt-struck", "fucktoy", "lunchmeat", "hot Cosby", "cum freak", "fuck face", "cum guzzler", "buckle buffer", "pussy fart", "clit licker", "smoke pole", "cunt hole", "bang hole", "bag ho", "cocksucker", "baby arm", "thundercunt", "cuntee", "snowball", "get any", "like fuck", "shit fucker", "purple headed solider man", "nut butter", "SPH", "slich", "cockbag", "anal impaler", "cuntsicle"]
+while True:
+	time.sleep(random.randint(5, 30));
+	keyboard.write(random.choice(a));
+	keyboard.press_and_release('enter');
+	
+//DeadPumpkin
+    
+    
+    
+    
+ 
